1733b93329fd226726c3cf6b42aa460e5efc21f4
Rosa Codina <rosa.codina90@gmail.com>
2022-08-23 12:19:42 +0200

    Organized folders

--- my exercises/bucle while HTML/bucleWhile.js
+++ my exercises/bucle while HTML/bucleWhile.js
@@ -1,12 +1,12 @@
 //Bucle while es como for, mientras se cumpla una condición se ejecutará el código que le digamos
 
-
-
 function bucleWhile(num) {
-    let i = 1;
+  let i = 1;
 
-    while (i < 11) {
-      console.log(i);
-      i++;
-    }
+  while (i < num) {
+    console.log(i);
+    i++;
+  }
 }
+
+bucleWhile(11);
